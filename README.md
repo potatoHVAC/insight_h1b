99045083078c9e0d0c503b014229f5a57ffb788a
Daniel Speer <dan.speer11@gmail.com>
2018-11-05 20:38:55 -0800

    final README.md

--- README.md
+++ README.md
@@ -6,17 +6,27 @@ A project for the Insight Data Science boot camp application.
 
 # Problem
 
-To clean and analyze the US Department of Labor’s Office of Foreign Labor Certification performance data. The analysis will output the top 10 occupations and the top 10 states for certified visa applications. 
+The US Department of Labor provides raw performance data for its Office of Foreign Labor Certification. The data needs to be cleaned and analyzed to find the top 10 certified jobs and the top 10 states where those jobs are performed. 
 
 # Approach
 
-#### Data Cleaning Assumptions:
-* SOC numbers with a decimal are subsets of their XX-XXX position and counted as the 
+1. Open target input file
+2. Clean the data as each row is checked.
+3. Collect the relevant data for certified applications that have valid data.
+4. Run analysis on the collected data.
+5. Save outputs to their respective files.
 
+#### Data Cleaning Assumptions:
+* SOC numbers with a decimal are subsets of their XX-XXX position and counted as the base job
+* Certifications with SOC numbers that are too short or too long are rejected entirely.
+* Certifications missing a job name but include a valid SOC number are not rejected from the count. If a second job is found with a matching SOC number then its job title will update the collected data point. 
+* Certifications missing a proper state code are rejected entirely.
+* Jobs with different SOC numbers but the same title are treated as separate jobs. 
+* Certifications are only accepted if the status is ‘CERTIFIED’ - no cleaning was applied to this section.
 
 # Run Instructions
 
-Placer input CSV file in the `./input/` directory. CSV must use `;` as delimiter. 
+Place input CSV file in the `./input/` directory. CSV must use `;` as delimiter. 
 
 The appropriate columns will be automatically collected if the following strings are included in the header. 
 
@@ -27,7 +37,7 @@ The appropriate columns will be automatically collected if the following strings
 
 # Output Information
 
-Two CSV files will be created/overwritten in the `./output/` folder.
+Two CSV files will be created in the `./output/` folder.
  
 * `./output/top_10_occupations.txt`
 * `./output/top_10_states.txt`
@@ -43,7 +53,7 @@ id;CASE_STATUS;YEAR_SOC;YEAR_SOC_NAME;WORKSITE_STATE
 4;CERTIFIED;11-1234;DOG WALKER;CA
 ```
 
-Then the output files will be:
+The output files will be:
 
 `./output/top_10_occupations.txt`:
 ```
@@ -60,6 +70,6 @@ WA;1;25
 ```
 # Future Improvements
 
-* Implement threading for the data collection to allow for files larger than the available memory.
+* Implement streaming for the data collection to allow for files larger than the available memory.
 * Both of the sorting methods are almost identical. Candidate for refactoring to follow DRY practices.
-
+* Use time stamps and input file names to automatically name output files to prevent accidental overriding and to allow for batch file analysis. 
--- insight_testsuite/temp/src/h1b_counting.py
+++ insight_testsuite/temp/src/h1b_counting.py
@@ -3,12 +3,18 @@ from re import match
 from os import walk
 
 def header_index(arg, header):
+    """Scan CSV header and return index of arg"""
     for i, ele in enumerate(header):
         if match(arg, ele):
             return i
     raise NameError('column not found')
 
 def populate_indices(header):
+    """Return dict of column indices based on CSV header
+
+    Arguments:
+    header -- the header string from the CSV
+    """
     indices = {}
     indices['status'] = header_index('.*STATUS.*', header)
     indices['soc'] = header_index('.*SOC_CODE.*', header)
@@ -17,6 +23,7 @@ def populate_indices(header):
     return indices     
     
 def check_soc_hyphen(soc):
+    """Return SOC with missing hypen added"""
     if len(soc) < 6:
         return soc
     soc_lst = list(soc)
@@ -29,15 +36,24 @@ def check_soc_hyphen(soc):
     return ''.join(soc_lst)
 
 def clean_soc(soc):
+    """Return cleaned and truncated SOC"""
     soc_strip = soc.replace(' ', '')
     soc_hyphen = check_soc_hyphen(soc_strip)
     return soc_hyphen[:7]
 
 def ratio_formatted(job_count, total):
+    """Return formatted ratio as a percentage"""
     num = round(100 * float(job_count) / total, 1)
     return str(num) + '%'
 
 def format_output(top_lst, header, total):
+    """Return formatted output string
+
+    Arguments:
+    top_lst -- top 10 jobs or states and their count as an array of tuples
+    header -- header string for the output
+    total -- total number of certified applications
+    """
     answer = [header]
     for job in top_lst:
         row = []
@@ -48,6 +64,11 @@ def format_output(top_lst, header, total):
     return "\n".join(answer)
 
 def sort_top_ten_states(dct):
+    """Return the top 10 states sorted by count then name
+    
+    Arguments:
+    dct -- Dict of states and their counts of certified applications
+    """
     sorted_dct = sorted(dct.items(), key=lambda kv: kv[1])
     
     sorted_alpha = []
@@ -60,8 +81,14 @@ def sort_top_ten_states(dct):
     
     return sorted_alpha[:10]
 
-def sort_top_ten_occupations(count, names):
-    sorted_dct = sorted(count.items(), key=lambda kv: kv[1])
+def sort_top_ten_occupations(counts, names):
+    """Return the top 10 occupations sorted by count then name
+    
+    Arguments:
+    counts -- Dict of occupation SOC and their counts of certified applications
+    names -- Dict of occupation SOC and their occupation title
+    """
+    sorted_dct = sorted(counts.items(), key=lambda kv: kv[1])
     
     sorted_alpha = []
     while len(sorted_alpha) < 10 and len(sorted_dct) > 0:
@@ -75,16 +102,27 @@ def sort_top_ten_occupations(count, names):
     return sorted_alpha[:10]
 
 def occupation_analysis(data):
+    """Return a string of the top 10 occupations 
+
+    Arguments:
+    data -- Dict that contains a dict of SOC and certified application counts, a dict of SOC and job title, and an integer of total certified applications
+    """
     top_ten = sort_top_ten_occupations(data['occupation_count'], data['occupation_dict'])   
     head = 'TOP_OCCUPATIONS;NUMBER_CERTIFIED_APPLICATIONS;PERCENTAGE'
     return format_output(top_ten, head, data['certified_count'])
         
 def state_analysis(data):
+    """Return a string of the top 10 occupations 
+
+    Arguments:
+    data -- Dict that contains a dict states and certified application counts and an integer of total certified applications
+    """
     top_ten = sort_top_ten_states(data['state_count'])
     head = 'TOP_STATES;NUMBER_CERTIFIED_APPLICATIONS;PERCENTAGE'
     return format_output(top_ten, head, data['certified_count'])
 
 def collect_data_points():
+    """Return dict of cleaned and valid data points"""
     files = []
     for (dirpath, dirnames, filenames) in walk('./input/'):
         files.extend(filenames)
@@ -130,6 +168,7 @@ def collect_data_points():
     }        
 
 def analyze_and_output(data):
+    """Run analysis modules and write them to the output files"""
     occupation_file = open('./output/top_10_occupations.txt', 'w')  
     occupation_file.write(occupation_analysis(data))
 
